4d314d1561f5a7b9a5ce6228d24d3d6ee3a1cda0
Justin Lowry <github@lowry.life>
2021-10-18 15:11:23 -0600

    Problem 10 done

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -172,13 +172,24 @@ console.log(arrayEvaluator)
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
 let loser = 'Glimmer'
 /*
-  Write a function that is called theEliminator, which takes in two arguments, contestants (which will each be an array of strings), and loser (which will be a string).
-  The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. Return the new contestants array.
+  Write a function that is called theEliminator, 
+  which takes in two arguments, contestants (which will each be an array of strings), 
+  nd loser (which will be a string).
+  The function should loop over the array of contestant names. 
+  If the loser string appears in the array, splice it out. Return the new contestants array.
 */
 
 //CODE HERE
-
-
+function theEliminator(contestants, loser) {
+  for (let i = 0; i < contestants.length; i++) {
+    if (contestants[i] === loser) {
+      console.log("removed" + contestants[i])
+      contestants.splice(i,1)
+    }
+  }
+  return contestants
+}
+console.log(theEliminator(contestants, loser))
 ////////////////// PROBLEM 11 ////////////////////
 let sampleString = "Hi, my name is Kylo."
 /*
